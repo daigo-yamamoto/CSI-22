d4feaa8acfe54fd907482d2ab8447ca04e5ddd1d
Jaja <bruno.daigo.yamamoto@gmail.com>
2023-03-29 09:53:39 -0300

    teste

---
+++ lista_1/3.py
@@ -0,0 +1,8 @@
+def sum_to(n):
+    soma = 0
+    for i in range(n + 1):
+        soma = soma + i
+    return soma
+
+
+print(sum_to(10))
---
+++ lista_1/4.py
@@ -0,0 +1,26 @@
+import turtle
+
+wn = turtle.Screen()
+wn.screensize(500, 500, 'light green')
+t = turtle.Turtle()
+t.color('blue')
+t.pensize(2)
+t.speed(100)
+
+t.penup()
+t.goto(-250, 0)
+t.pendown()
+
+for i in range(200):
+    t.fd(i*2)
+    t.rt(90)
+
+t.penup()
+t.goto(250, 0)
+t.pendown()
+
+for i in range(200):
+    t.fd(i*2)
+    t.rt(89.5)
+
+wn.mainloop()
